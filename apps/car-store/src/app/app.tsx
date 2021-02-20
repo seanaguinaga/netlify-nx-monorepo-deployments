093462e0eb3eefacc53648041e3c1e3ccc054b06
Sean Aguinaga <aguinaga.sean@gmail.com>
2021-02-19 18:31:51 -0800

    Update app.tsx

--- apps/car-store/src/app/app.tsx
+++ apps/car-store/src/app/app.tsx
@@ -17,7 +17,7 @@ export const App = () => {
     <div className="app">
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1>Welcome to new car-store!</h1>
+        <h1>Welcome to car-store!</h1>
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>
